6dd07fad67f99c158579a08e655e740d6016ac3c
Ankit <sinhaankit043@gmail.com>
2023-08-05 01:29:18 +0530

    initial commit

---
+++ client/src/hooks/useCategory.js
@@ -0,0 +1,25 @@
+import { REST_API } from "./helper";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useCategory() {
+  const [categories, setCategories] = useState([]);
+
+  //get cat
+  const getCategories = async () => {
+    try {
+      const { data } = await axios.get(
+        `${REST_API}/api/v1/category/get-category`
+      );
+      setCategories(data?.category);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  return categories;
+}
